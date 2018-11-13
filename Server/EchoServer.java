c783c92da46e7d12287f1287a7efb034566ee619
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-11-12 19:42:23 -0800

    added comments and added .java extension

--- Server/EchoServer.java
+++ Server/EchoServer.java
@@ -2,9 +2,6 @@
 import java.net.BindException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.Date;
-import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,28 +11,37 @@ import java.util.Scanner;
 
 /**
  *
- * @author lukecjm
+ * @author Luke Clements and Ryan Cole
  */
-public class EchoServerServer {
+public class EchoServerServer 
+{
     public static void main( String args[]) throws Exception 
     {
         DatagramSocket dsock;
+        
+        //sets up the byte array for the data and packet for the array of the specified size
         byte bytes[] = new byte[256];                                
         DatagramPacket dpack = new DatagramPacket(bytes, bytes.length );
         
         try
         {
+            //sets up a UDP socket at the passed port number to listen for incoming data
             dsock = new DatagramSocket(Integer.parseInt(args[0]));
 
             while(true) 
             {
+                //receives data
                 dsock.receive(dpack);
 
+                //gets data from the datagramPacket and converts it to a string
                 byte messageBytes[] = dpack.getData();
                 int packSize = dpack.getLength();
                 String message = new String(messageBytes, 0, packSize);
 
+                //prints the message received
                 System.out.println(message);
+                
+                //sends the packet back to the sender
                 dsock.send(dpack);
             }
         }
